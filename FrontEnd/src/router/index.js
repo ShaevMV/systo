7073d0eafa4966741647b1c19fd2d813dcce590f
shaevmv <ShaevMV@gmail.com>
2023-02-04 17:27:05 +0300

    fix menu

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -31,7 +31,7 @@ const routes = [
     },
     {
         path: '/forgotPassword',
-        name: 'registration',
+        name: 'forgotPassword',
         component: RegView,
         meta: {
             'guest': true
