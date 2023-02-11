0a7dd0e6e8d809fe59ba0434dbafb8cb30f1bd57
shaevmv <ShaevMV@gmail.com>
2023-02-11 17:03:49 +0300

    Фикс бага

--- Backend/src/Ticket/CreateTickets/Domain/ProcessCreateTicket.php
+++ Backend/src/Ticket/CreateTickets/Domain/ProcessCreateTicket.php
@@ -8,6 +8,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Throwable;
 use Tickets\Shared\Domain\Bus\EventJobs\DomainEvent;
 use Tickets\Shared\Domain\ValueObject\Uuid;
@@ -15,7 +16,7 @@ use Tickets\Ticket\CreateTickets\Application\TicketApplication;
 
 class ProcessCreateTicket implements ShouldQueue, DomainEvent
 {
-    use Dispatchable, InteractsWithQueue, Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function __construct(
         private Uuid $orderId,
