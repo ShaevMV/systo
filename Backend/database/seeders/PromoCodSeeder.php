09fa89edddd1115eb201bdd6625105e0434b6afb
shaevmv <ShaevMV@gmail.com>
2023-02-20 11:31:04 +0300

    сменил цену для промокода

--- Backend/database/seeders/PromoCodSeeder.php
+++ Backend/database/seeders/PromoCodSeeder.php
@@ -12,7 +12,7 @@ class PromoCodSeeder extends Seeder
 {
     public const ID_FOR_SYSTO = '2ecd9108-12c0-4ef1-9095-917442673a3c';
     public const NAME_FOR_SYSTO = 'SYSTO20';
-    public const DISCOUNT_FOR_SYSTO = 800;
+    public const DISCOUNT_FOR_SYSTO = 600;
 
     /**
      * Run the database seeds.
