3f753eb4e97265cd2adaefabb977fa53011a46fd
shaevmv <ShaevMV@gmail.com>
2023-02-21 14:55:38 +0300

    поставил ссылку на билеты

--- Backend/resources/views/email/orderToPaid.blade.php
+++ Backend/resources/views/email/orderToPaid.blade.php
@@ -43,7 +43,7 @@
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Спасибо тебе за твой вклад в создании общего праздника!</p>
                         <br/>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Электронные билеты с qr-кодом на каждого гостя прикреплены к этому письму.</p>
-                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">А также доступны в разделе Мои оргвзносы {{ env('APP_FRONT_URL') }}/myOrders</p>
+                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">А также доступны в разделе Мои оргвзносы http://org.solarsysto.ru/myOrders</p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">На входе на фестиваль каждому гостю необходимо будет показать свой qr-код на экране телефона или в распечатанном виде! Позаботься об этом заранее!</p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">До встречи, на фестивале!</p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">C любовью, команда организаторов Solar Systo Togathering</p>
