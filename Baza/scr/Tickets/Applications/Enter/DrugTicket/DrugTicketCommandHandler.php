ade2293d56b132f0fc262ffe6c210d9fdcd6dc51
shaevmv <ShaevMV@gmail.com>
2023-05-16 15:11:51 +0300

    fix

--- Baza/scr/Tickets/Applications/Enter/DrugTicket/DrugTicketCommandHandler.php
+++ Baza/scr/Tickets/Applications/Enter/DrugTicket/DrugTicketCommandHandler.php
@@ -3,6 +3,7 @@
 namespace Baza\Tickets\Applications\Enter\DrugTicket;
 
 use Baza\Shared\Domain\Bus\Command\CommandHandler;
+use Baza\Tickets\Applications\Enter\SpisokTicket\SpisokTicketCommand;
 use Baza\Tickets\Repositories\ElTicketsRepositoryInterface;
 use Baza\Tickets\Repositories\FriendlyTicketRepositoryInterface;
 use Baza\Tickets\Repositories\SpisokTicketsRepositoryInterface;
@@ -16,7 +17,7 @@ class DrugTicketCommandHandler implements CommandHandler
     {
     }
 
-    public function __invoke(DrugTicketCommand $command): void
+    public function __invoke(SpisokTicketCommand $command): void
     {
         $this->repository->skip($command->getId(), $command->getChangeId());
     }
