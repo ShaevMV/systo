73ce4009332f90f6e82bdeed975de9ba2e52f0e2
shaevmv <ShaevMV@gmail.com>
2023-05-16 09:48:43 +0300

    Добавил вывод кол-во билетов в списках

--- List/resources/views/admin/tickets.blade.php
+++ List/resources/views/admin/tickets.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+            <p> Всего билетов: <b>{{count($tickets)}}</b></p>
             <table class="table">
                 <thead>
                 <tr>
