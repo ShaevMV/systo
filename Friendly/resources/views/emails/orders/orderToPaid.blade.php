7c22f372c7c152b7d38191ca88e6ea3a3e3a9ebd
shaevmv <ShaevMV@gmail.com>
2023-06-24 16:58:34 +0300

    fix mail

--- Friendly/resources/views/emails/orders/orderToPaid.blade.php
+++ Friendly/resources/views/emails/orders/orderToPaid.blade.php
@@ -31,7 +31,7 @@
                     <td style="padding: 20px;">
                         <table style="border: none; border-collapse: collapse;" border="0" cellpadding="0" cellspacing="0">
                             <tr>
-                                <td style="padding-left: 20px; font-size: 30px; font-family: Tahoma, sans-serif;">Оргвзнос на Solar Systo Togathering {{date('Y')}} подтверждён</td>
+                                <td style="padding-left: 20px; font-size: 30px; font-family: Tahoma, sans-serif;">Оргвзнос на Систо-Осень 2023 подтверждён</td>
                             </tr>
                         </table>
                     </td>
@@ -39,7 +39,7 @@
                 <tr>
                     <td style="padding: 10px 20px 20px 20px; border-top: 1px solid #e6e6e6;  border-bottom: 1px solid #e6e6e6;">
                         <p style="text-align: center; font-family: Tahoma, sans-serif; font-size: 24px; font-weight: 500;">Привет, дорогой друг</p>
-                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Твой оргвзнос на Solar Systo Togathering {{date('Y')}} потдверждён</p>
+                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Твой оргвзнос на Систо-Осень 2023 потдверждён</p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Спасибо тебе за твой вклад в создании общего праздника!</p>
                         <br/>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Электронные билеты с qr-кодом на каждого гостя прикреплены к этому письму.</p>
