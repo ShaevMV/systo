4d2ba29c382f8c81c50c58ad55ee55e808e8ca5a
shaevmv <ShaevMV@gmail.com>
2023-09-05 17:11:14 +0300

    добавил push только на осень

--- Friendly/app/Console/Commands/PushTickets.php
+++ Friendly/app/Console/Commands/PushTickets.php
@@ -41,7 +41,7 @@ class PushTickets extends Command
         TicketService $ticketService
     )
     {
-        $tickets = FriendlyTicket::all();
+        $tickets = FriendlyTicket::where('festival_id','=',env('UUID_SECOND_FESTIVAL','9d679bcf-b438-4ddb-ac04-023fa9bff4b3'))->get();
 
         foreach ($tickets as $ticket) {
             $ticketService->pushTicketFriendly($ticket);
