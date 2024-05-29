1f3324b2daf9ac7085ab66601ddf860fe02cc67c
shaevmv <shaevmv@gmail.com>
2024-05-29 11:57:32 +0300

    [drug] добавил базу

--- Friendly/routes/api.php
+++ Friendly/routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::middleware('bot')->post('/insert/list', [ApiTicketController::class, 'insert']);
-Route::middleware('bot')->post('/getFestival', [ApiTicketController::class, 'getFestival']);
+Route::middleware('bot')->get('/getFestival', [ApiTicketController::class, 'getFestival']);
