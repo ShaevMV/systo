58f6c9d77f41045faa73a28e34c667b7267d8189
shaevmv <shaevmv@gmail.com>
2025-05-04 16:53:15 +0300

    [friendly] fix

--- Friendly/resources/views/admin/users.blade.php
+++ Friendly/resources/views/admin/users.blade.php
@@ -7,7 +7,7 @@
                 </x-jet-responsive-nav-link>
             </div>
             <h3> ИТОГО: </h3>
-            <table class="table" style="width: 800px;">
+            <table class="table" style="width: 1136px;">
                 <thead>
                 <tr>
                     <th>{{ __('Сумма за френдли') }}</th>
@@ -21,10 +21,10 @@
                 </thead>
                 <tbody>
                 <tr>
-                    <td><b>{{number_format($total['friendly']['sum'] ?? 0, 2, '.', ' ')}}</b></td>
+                    <td style="width: 200px;"><b>{{number_format($total['friendly']['sum'] ?? 0, 2, '.', ' ')}}</b></td>
                     <td><b>{{$total['friendly']['count'] ?? 0}}</b></td>
                     <td><b>{{$total['friendly']['count_is_need_seedling'] ?? 0}}</b></td>
-                    <td><b>{{number_format($total['live']['sum'] ?? 0, 2, '.', ' ')}}</b></td>
+                    <td style="width: 200px;"><b>{{number_format($total['live']['sum'] ?? 0, 2, '.', ' ')}}</b></td>
                     <td><b>{{$total['live']['count'] ?? 0}}</b></td>
                     <td><b>{{$total['list']['count'] ?? 0}}</b></td>
                     <td style="width: 200px;"><b>{{number_format(($total['live']['sum'] + $total['friendly']['sum']), 2, '.', ' ')}}</b></td>
