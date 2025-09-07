75e360e2e706183941eed243ec4735ec87a060e5
shaevmv <shaevmv@gmail.com>
2025-09-07 21:34:37 +0300

    [vhod] добавил тульского

--- Baza/app/Console/Commands/CreateUserCommand.php
+++ Baza/app/Console/Commands/CreateUserCommand.php
@@ -33,49 +33,9 @@ class CreateUserCommand extends Command
     {
         $users = [
             [
-                "name" => "Катя_Арчи",
-                "email" => "Katya_Archi@spaceofjoy.ru",
-                "password" => "znaytvse37",
-            ],
-            [
-                "name" => "Марина_фауст",
-                "email" => "Marina_faust@spaceofjoy.ru",
-                "password" => "znaytvse36",
-            ],
-            [
-                "name" => "Никита_фауст",
-                "email" => "Nikita_faust@spaceofjoy.ru",
-                "password" => "znaytvse35",
-            ],
-            [
-                "name" => "Саша_ядя",
-                "email" => "Sasha_poison@spaceofjoy.ru",
-                "password" => "znaytvse34",
-            ],
-            [
-                "name" => "Маша_ядя",
-                "email" => "Masha_yadya@spaceofjoy.ru",
-                "password" => "znaytvse33",
-            ],
-            [
-                "name" => "Аля фауст",
-                "email" => "Alla Faust@spaceofjoy.ru",
-                "password" => "znaytvse32",
-            ],
-            [
-                "name" => "Алексей",
-                "email" => "Alexey@spaceofjoy.ru",
-                "password" => "znaytvse31",
-            ],
-            [
-                "name" => "Джордж_ядя",
-                "email" => "George_poison@spaceofjoy.ru",
-                "password" => "znaytvse30",
-            ],
-            [
-                "name" => "Цукерок_мария",
-                "email" => "Tsukerok_Maria@spaceofjoy.ru",
-                "password" => "znaytvse29",
+                "name" => "Tulskiy",
+                "email" => "Tulskiy@spaceofjoy.ru",
+                "password" => "znaytvse57",
             ],
         ];
 
