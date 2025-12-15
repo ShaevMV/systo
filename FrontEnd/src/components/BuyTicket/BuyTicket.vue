3640652434e673d0b34e09ae1c106035bc185831
shaevmv <shaevmv@gmail.com>
2025-12-15 09:52:39 +0300

    [front] вынис QuestionnaireTicket

--- FrontEnd/src/components/BuyTicket/BuyTicket.vue
+++ FrontEnd/src/components/BuyTicket/BuyTicket.vue
@@ -426,106 +426,7 @@
                     ></textarea>
                   </div>
                 </div>
-                <div class="pp1 row">
-                  <span>ШАГ 4.</span> Заполни анкетные данные для подтверждения участия в туристическом слёте:
-                </div>
-                <div class="mb-12">
-                  <div class="col-3">
-                    <label for="questionnaire_agy">Возраст: *</label>
-                  </div>
-
-                  <div class="col-4">
-                    <div class="input-group" id="promo-input">
-                      <input
-                          type="number"
-                          id="questionnaire_agy"
-                          class="form-control"
-                          placeholder="Возраст"
-                          aria-label="Возраст"
-                          v-model="questionnaire.agy"
-                          aria-describedby="basic-addon1"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <label for="questionnaire_tellegram">Telegram-аккаунт:</label>
-                  </div>
-
-                  <div class="col-4">
-                    <div class="input-group" id="promo-input">
-                      <input
-                          type="text"
-                          id="questionnaire_tellegram"
-                          class="form-control"
-                          placeholder="Telegram-аккаунт"
-                          aria-label="Telegram-аккаунт"
-                          v-model="questionnaire.telegram"
-                          aria-describedby="basic-addon1"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <label for="questionnaire_vk">Профайл Вконтакте:</label>
-                  </div>
-
-                  <div class="col-4">
-                    <div class="input-group" id="promo-input">
-                      <input
-                          type="text"
-                          id="questionnaire_tellegram"
-                          class="form-control"
-                          placeholder="Профайл Вконтакте"
-                          aria-label="Профайл Вконтакте"
-                          v-model="questionnaire.vk"
-                          aria-describedby="basic-addon1"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <label for="questionnaire_howManyTimes">Сколько раз ты уже бывал на Систо? *</label>
-                  </div>
-
-                  <div class="col-4">
-                    <div class="input-group" id="promo-input">
-                      <input
-                          type="number"
-                          id="questionnaire_howManyTimes"
-                          class="form-control"
-                          placeholder="Сколько раз ты уже бывал на Систо? *"
-                          aria-label="Сколько раз ты уже бывал на Систо? *"
-                          v-model="questionnaire.howManyTimes"
-                          aria-describedby="basic-addon1"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="col-3">
-                    <label for="questionnaire_musicStyles">Стили музыки, которые предпочитаешь в лесу:</label>
-                  </div>
-
-                  <div class="col-9">
-                    <textarea
-                        class="form-control order-text"
-                        v-model="questionnaire.musicStyles"
-                        id="questionnaire_musicStyles"
-                    ></textarea>
-                  </div>
-
-                  <div class="col-3">
-                    <label for="questionnaire_questionForSysto">Ответь кратко и честно на простой вопрос "Зачем ты едешь на Систо?": *</label>
-                  </div>
-
-                  <div class="col-9">
-                    <textarea
-                        class="form-control order-text"
-                        v-model="questionnaire.questionForSysto"
-                        id="questionnaire_musicStyles"
-                    ></textarea>
-                  </div>
-                </div>
+                <questionnaire-ticket :questionnaire="questionnaire"/>
                 <div class="row mt-4">
                   <div class="form-check" id="check-check">
                     <input
@@ -652,9 +553,11 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import QuestionnaireTicket from "@/components/BuyTicket/QuestionnaireTicket.vue";
 
 export default {
   name: 'BuyTicket',
+  components: {QuestionnaireTicket},
   data() {
     return {
       preload: false,
