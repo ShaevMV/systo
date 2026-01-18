3331359e9ea9eee7fd6572c2f876853efd46eed1
shaevmv <shaevmv@gmail.com>
2026-01-18 11:21:48 +0300

    [org] правки

--- Backend/src/Ticket/CreateTickets/Repositories/InMemoryMySqlTicketsRepository.php
+++ Backend/src/Ticket/CreateTickets/Repositories/InMemoryMySqlTicketsRepository.php
@@ -154,7 +154,7 @@ class InMemoryMySqlTicketsRepository implements TicketsRepositoryInterface
             $result['city'],
             $result['last_comment'],
             Carbon::parse($result['created_at']),
-            empty($result['pdf']) ? null : $result['pdf'],
+            empty(trim($result['pdf'])) ? null : $result['pdf'],
             $result['emailView'],
             new Uuid($result['festival_id']),
             in_array($result['ticket_type_id'], (array)['222abc0c-fc8e-4a1d-a4b0-d345cafada10']),
