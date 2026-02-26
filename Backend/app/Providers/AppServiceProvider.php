bffc9f04db1946542096352567223bf5b7bb3ec6
shaevmv <shaevmv@gmail.com>
2026-02-27 01:58:52 +0300

    [org] поппытка подключить логер

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
+use Sentry\SentrySdk;
+use Sentry\ClientBuilder;
+use Sentry\State\Hub;
+use Sentry\Monolog\Handler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +21,27 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->isLocal()) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+
+// Регистрируем канал sentry_logs
+        Log::extend('sentry_logs', function ($app, $config) {
+            // Создаем клиент Sentry
+            $client = ClientBuilder::create([
+                'dsn' => env('SENTRY_LARAVEL_DSN'),
+                'environment' => env('APP_ENV', 'production'),
+            ])->getClient();
+
+            // Создаем Hub с клиентом
+            $hub = new Hub($client);
+
+            // Устанавливаем Hub в качестве текущего
+            SentrySdk::setCurrentHub($hub);
+
+            // Создаем Monolog handler с Hub
+            $handler = new Handler($hub);
+
+            // Возвращаем Monolog логгер
+            return new \Monolog\Logger('sentry', [$handler]);
+        });
     }
 
     /**
