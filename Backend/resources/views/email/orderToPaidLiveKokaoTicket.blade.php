4ba7ec8dae1fb83bcf2533b3b2405b0f05c80fc9
shaevmv <shaevmv@gmail.com>
2026-03-05 16:08:09 +0300

    [org] добавил вывод номера заказа для живых билетов

--- Backend/resources/views/email/orderToPaidLiveKokaoTicket.blade.php
+++ Backend/resources/views/email/orderToPaidLiveKokaoTicket.blade.php
@@ -39,7 +39,7 @@
                 <tr>
                     <td style="padding: 10px 20px 20px 20px; border-top: 1px solid #e6e6e6;  border-bottom: 1px solid #e6e6e6;">
                         <p style="text-align: center; font-family: Tahoma, sans-serif; font-size: 24px; font-weight: 500;">Привет, дорогой друг</p>
-                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Твой оргвзнос на {{$festivalName}} потдверждён</p>
+                        <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Твой заказ №{{$kilter}} подтверждён.  </p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Спасибо тебе за твой вклад в создании общего праздника!</p>
                         <p style="font-family: Tahoma, sans-serif; font-size: 18px;">Ты можете получить свой конверт с аналоговой картой и стикером в пространстве "Cacao Temple" в Санкт-Петербурге по адресу: м.Сенная Площадь, Садовая ул., 107<br>
                             Кафе открыто с 12.00 до 22.00. <b>Чашка какао в подарок</b></p>
