1c990219075012ecac245df2db8e0875faf413ec
shaevmv <shaevmv@gmail.com>
2026-03-11 19:36:50 +0300

    [org] добавил вывод ссылку вприглошение для всех кто выше гостя

--- Backend/src/Order/OrderTicket/Service/InviteLinkService.php
+++ Backend/src/Order/OrderTicket/Service/InviteLinkService.php
@@ -8,21 +8,27 @@ use Shared\Domain\ValueObject\Uuid;
 use Shared\Questionnaire\Domain\ValueObject\QuestionnaireStatus;
 use Shared\Questionnaire\Repositories\QuestionnaireRepositoryInterface;
 use Tickets\Order\OrderTicket\Repositories\InviteLinkRepositoryInterface;
+use Tickets\User\Account\Helpers\AccountRoleHelper;
 
 class InviteLinkService
 {
     private const LINK_PATCH = 'https://org.spaceofjoy.ru/invite/';
 
     public function __construct(
-        private InviteLinkRepositoryInterface $repository,
+        private InviteLinkRepositoryInterface    $repository,
         private QuestionnaireRepositoryInterface $questionnaireRepository,
     )
     {
     }
 
-    public function getLink(Uuid $userId): ?string
+    public function getLink(Uuid $userId, string $role): ?string
     {
-        return $this->repository->isPaidOrderByUserId($userId) ? self::LINK_PATCH . $userId->value() : null;
+        if (AccountRoleHelper::guest === $role) {
+            return $this->repository->isPaidOrderByUserId($userId) ? self::LINK_PATCH . $userId->value() : null;
+        } else {
+            return self::LINK_PATCH . $userId->value();
+        }
+
     }
 
     public function isPaidOrderByUserId(Uuid $userId, string $email): bool
